40664ed3418f5b17c8472cc0da3e80b1794894e0
Veronika Polonchak <polonchakveronika@gmail.com>
2022-05-19 17:26:30 +0200

    data.sql añadido

---
+++ src/main/resources/data.sql
@@ -0,0 +1,47 @@
+	drop table if exists asignado;
+	drop table if exists cientificos;
+	drop table if exists proyecto;
+	
+	create table proyectos (
+	id varchar(4) primary key, 
+	nombre varchar(255),
+	horas int
+	);
+	
+	create table cientificos (
+	id varchar(8) primary key,
+	nombre varchar(255)
+	);
+	
+	create table asignado (
+	id int primary key auto_increment,
+	cientifico varchar(8),
+	proyecto varchar(4),
+	foreign key(cientifico) references cientificos(id)
+	on delete cascade
+	on update cascade,
+	foreign key(proyecto) references proyectos(id)
+	on delete cascade
+	on update cascade
+	);
+	
+	insert into proyectos values
+	('1234', "Proyecto1", 10),
+	('2234', "Proyecto2", 10),
+	('3234', "Proyecto3", 10),
+	('4234', "Proyecto4", 10),
+	('5234', "Proyecto5", 10);
+	
+	insert into cientificos values
+	("12345678", "Veronika"),
+	("22345678", "Isidre"),
+	("32345678", "Adria"),
+	("42345678", "Marc"),
+	("52345678", "Joan");
+	
+	insert into asignado (cientifico, proyecto) values
+	("12345678", "1234"),
+	("22345678", "2234"),
+	("32345678", "3234"),
+	("42345678", "4234"),
+	("52345678", "5234");
